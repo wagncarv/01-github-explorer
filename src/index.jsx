4b3194230df7120bffb621a80439908c29429a58
Wagner Carvalho <wagncarv@gmail.com>
2021-08-14 20:45:57 -0300

    configuracao webpack, dev server

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
+import { render } from 'react-dom';
 
 import { App } from './App';
 
-function Wrapper(){
-    return <h1>Hello world</h1>
-}
+render(<App />, document.getElementById('root'))
